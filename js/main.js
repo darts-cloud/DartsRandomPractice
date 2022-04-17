347c6f1fab55b037f6669c7abe8257aef1854867
NAOKI <t_f_revolution@hotmail.com>
2022-04-17 20:44:10 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(function(){
             target = "DB";
         }
         $("#dart" + i).text(target);
-        db.drawColor(target, "yellow");
+        db.drawColor(target, "#DDDD33");
     }
     db.refresh();
 });
